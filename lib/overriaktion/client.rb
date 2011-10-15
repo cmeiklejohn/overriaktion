c24f1dc56c0a661ff2cb740a7ca47b4cafb10526
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-10-15 02:34:40 -0400

    Refactor request into class and instantiate one member.

--- lib/overriaktion/client.rb
+++ lib/overriaktion/client.rb
@@ -1,22 +1,28 @@
 module Overriaktion
   class Client
+    attr_accessor :request 
+
+    def initialize 
+      @request = Request.new
+    end
+
     def riak_clusters
-      response = Request.get("/riak_clusters.json")
+      response = @request.get("/riak_clusters.json")
       response.map { |riak_cluster| RiakCluster.new(riak_cluster) }
     end
 
     def riak_cluster(riak_cluster_id)
-      response = Request.get("/riak_clusters/#{riak_cluster_id}.json")
+      response = @request.get("/riak_clusters/#{riak_cluster_id}.json")
       RiakCluster.new(response)
     end
 
     def riak_nodes(riak_cluster_id)
-      response = Request.get("/riak_clusters/#{riak_cluster_id}/riak_nodes.json")
+      response = @request.get("/riak_clusters/#{riak_cluster_id}/riak_nodes.json")
       response.map { |riak_node| RiakNode.new(riak_node) }
     end
 
     def riak_node(riak_cluster_id, riak_node_id)
-      response = Request.get("/riak_clusters/#{riak_cluster_id}/riak_nodes/#{riak_node_id}.json")
+      response = @request.get("/riak_clusters/#{riak_cluster_id}/riak_nodes/#{riak_node_id}.json")
       RiakNode.new(response)
     end
   end
--- lib/overriaktion/request.rb
+++ lib/overriaktion/request.rb
@@ -1,9 +1,13 @@
 module Overriaktion
-  module Request
+  class Request
     include HTTParty
 
     base_uri 'dont.overriak.com'
 
     format :json
+
+    def get(uri) 
+      self.class.get(uri)
+    end
   end
 end
