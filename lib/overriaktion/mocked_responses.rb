22b2094b6d8fc345608a80374fb67720dabc1b9b
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-10-15 01:56:25 -0400

    Add single response for riak cluster.

--- lib/overriaktion/mocked_responses.rb
+++ lib/overriaktion/mocked_responses.rb
@@ -1,5 +1,6 @@
 module Overriaktion
   module MockedResponses
-    RIAK_CLUSTERS = '[{"created_at":"2011-10-10T03:31:34Z","id":1,"name":"Localhost","updated_at":"2011-10-10T03:31:34Z","user_id":1}]'
+    RIAK_CLUSTER = '{"created_at":"2011-10-10T03:31:34Z","id":1,"name":"Localhost","updated_at":"2011-10-10T03:31:34Z","user_id":1}'
+    RIAK_CLUSTERS = "[#{RIAK_CLUSTER}]"
   end
 end
