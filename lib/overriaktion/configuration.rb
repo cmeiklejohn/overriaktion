c8e9f63b7e940f5f2118c03105b58fe62e9f9bad
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-10-16 10:57:50 -0400

    Delegate class methods to singleton instance; define alias for api_host.

--- lib/overriaktion/configuration.rb
+++ lib/overriaktion/configuration.rb
@@ -5,5 +5,14 @@ module Overriaktion
     VALID_ATTRIBUTES = [:api_key, :api_host]
 
     attr_accessor *VALID_ATTRIBUTES
+
+    def api_host 
+      @api_host || EndPoints::DEFAULT
+    end
+
+    def self.method_missing(method, *args, &block)
+      return super unless instance.respond_to?(method)
+      instance.send(method, *args, &block)
+    end
   end
 end
