179962af6520d68f1f2de3d867984f56f97899cc
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-10-29 23:09:24 -0400

    Now we've got a good set of fixtures, set record to disabled.

--- lib/overriaktion/vcr.rb
+++ lib/overriaktion/vcr.rb
@@ -3,6 +3,7 @@
 require 'vcr'
 
 VCR.config do |c|
-  c.cassette_library_dir = 'fixtures/vcr_cassettes'
   c.stub_with :webmock
+  c.cassette_library_dir = 'fixtures/vcr_cassettes'
+  c.default_cassette_options = { :record => :none }
 end
