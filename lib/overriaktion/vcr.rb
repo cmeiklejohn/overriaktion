254edd616c9b6f535d301f40c30c085d089692d7
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-10-29 23:31:22 -0400

    Load VCR if running in the test environment.

--- lib/overriaktion/vcr.rb
+++ lib/overriaktion/vcr.rb
@@ -2,8 +2,12 @@
 
 require 'vcr'
 
-VCR.config do |c|
-  c.stub_with :webmock
-  c.cassette_library_dir = 'fixtures/vcr_cassettes'
-  c.default_cassette_options = { :record => :none }
+if ENV['RAILS_ENV'] == 'test'
+  PROJECT_ROOT = File.expand_path("../../..", __FILE__)
+  
+  VCR.config do |c|
+    c.stub_with :webmock
+    c.cassette_library_dir = "#{PROJECT_ROOT}/fixtures/vcr_cassettes"
+    c.default_cassette_options = { :record => :none }
+  end
 end
