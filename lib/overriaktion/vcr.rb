da133ae12007e138358bc6041866bb1cf7ffa0c4
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-10-31 23:31:46 -0400

    Record new episodes.

--- lib/overriaktion/vcr.rb
+++ lib/overriaktion/vcr.rb
@@ -6,6 +6,6 @@ if ENV['RAILS_ENV'] == 'test'
   VCR.config do |c|
     c.stub_with :webmock
     c.cassette_library_dir = "#{File.expand_path('../../..', __FILE__)}/fixtures/vcr_cassettes"
-    c.default_cassette_options = { :record => :none }
+    c.default_cassette_options = { :record => :new_episodes }
   end
 end
