cf8aa2b4c2fea3a9f72adaf0d1f4970ab6f8a25b
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-10-31 22:31:59 -0400

    Don't reference the same constant differently.

--- lib/overriaktion/vcr.rb
+++ lib/overriaktion/vcr.rb
@@ -3,11 +3,9 @@
 require 'vcr'
 
 if ENV['RAILS_ENV'] == 'test'
-  PROJECT_ROOT = File.expand_path("../../..", __FILE__)
-  
   VCR.config do |c|
     c.stub_with :webmock
-    c.cassette_library_dir = "#{PROJECT_ROOT}/fixtures/vcr_cassettes"
+    c.cassette_library_dir = "#{File.expand_path('../../..', __FILE__)}/fixtures/vcr_cassettes"
     c.default_cassette_options = { :record => :none }
   end
 end
